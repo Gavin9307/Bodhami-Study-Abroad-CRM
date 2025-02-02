c2db3c55d7bf39062d769a8de692c010220c0dab
Gavin9307 <gavindacosta9307@gmail.com>
2025-02-03 00:52:06 +0530

    Add Country Setup and Import Form Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,17 @@ import { AboutComponent } from './about/about.component';
 import { ProfileUpdateComponent } from './account/profile-update/profile-update.component';
 import { CountrySetupComponent } from './study-abroad-setup/country-setup/country-setup.component';
 import { CourseSetupComponent } from './study-abroad-setup/course-setup/course-setup.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { AddCountryComponent } from './study-abroad-setup/add-country/add-country.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -22,6 +32,7 @@ const routes: Routes = [
   { path: 'abouts', component: AboutComponent },
   { path: 'account/profile-update', component: ProfileUpdateComponent },
   { path: 'study-abroad-setup/country-setup', component: CountrySetupComponent },
+  { path: 'study-abroad-setup/country-setup/add-country', component: AddCountryComponent },
   { path: 'study-abroad-setup/course-setup', component: CourseSetupComponent },
   { path: '**', redirectTo: '/home' }
 ];
@@ -37,13 +48,24 @@ const routes: Routes = [
     AboutComponent,
     ProfileUpdateComponent,
     CountrySetupComponent,
-    CourseSetupComponent
+    CourseSetupComponent,
+    AddCountryComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatExpansionModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule  
   ],
   exports: [RouterModule],
   providers: [],
