e5c16181bc3e010ed55c1ee09873e70f85007b91
Gavin9307 <gavindacosta9307@gmail.com>
2025-04-14 16:43:43 +0530

    update add student

--- src/app/student-management/student-add/student-add.component.ts
+++ src/app/student-management/student-add/student-add.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-student-add',
@@ -7,18 +9,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  isLoading: Boolean = false;
+
+  firstName: String = "";
+  lastName: String = "";
+  middleName: String = "";
+  dateOfBirth: String = "";
+  email: String = "";
+  username: String = "";
+  password: String = "";
+  phoneNo: String = "";
+  pic: String = "";
+  isDeleted: Boolean = false;
+
+
+  addStudent() {
+    const formattedDOB = this.dateOfBirth instanceof Date
+      ? this.dateOfBirth.toISOString().split('T')[0]
+      : this.dateOfBirth; // assumes string is already formatted
+  
+    this.isLoading = true;
+  
+    this.http.post("http://localhost:8080/api/user/add", {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      middleName: this.middleName,
+      dateOfBirth: formattedDOB,
+      email: this.email,
+      username: this.username,
+      password: this.password,
+      phoneNo: this.phoneNo,
+      pic: this.pic,
+      isDeleted: this.isDeleted
+    })
+    .subscribe(
+      (response) => {
+        this.toastr.success("Student added successfully");
+        this.isLoading = false;
+      },
+      (error) => {
+        this.toastr.error("Student cannot be added");
+        this.isLoading = false;
+      }
+    );
   }
-
-  student = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phoneNumber: '',
-    password: '',
-    course: ''
-  };
+  
 
 }
